2f426bc90b63b17e4b87938a0750bbbd2ab6c6d3
cy <cyliimdev@gmail.com>
2020-08-10 21:34:00 +1200

    Create tower.js

---
+++ tower/tower.js
@@ -0,0 +1,19 @@
+async function start() {
+    console.log("debug")
+    let x = JSON.parse(document.getElementById("first").value)
+    let y = JSON.parse(document.getElementById("second").value)
+
+    let i;
+
+    for (i = 0; i < document.getElementById("iterations").value; i++) {
+
+        let z = x + y
+        var para = await document.createElement("li");
+        var node = await document.createTextNode(z);
+        para.appendChild(node);
+        document.getElementById("output1").appendChild(para);
+        x = y
+        y = z
+
+    }
+}
