fe1afa6f1a4bc4492635dfa2570528eaeffe3529
cy <cyliimdev@gmail.com>
2024-05-05 19:00:51 +1200

    Update script.js

--- shakespear/script.js
+++ shakespear/script.js
@@ -3,6 +3,7 @@ let two = ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-br
 let three = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]
 
 function clear() {
+  console.log("Hello world!")
   let oneOne = one[Math.floor(Math.random() * one.length)]
   let twoTwo = two[Math.floor(Math.random() * two.length)]
   let threeThree = three[Math.floor(Math.random() * three.length)]
